d84ccb0da6e5e1a6d10a72be508b7bffa7e5108b
Flc゛ <four_leaf_clover@foxmail.com>
2022-05-21 21:07:58 +0800

    str_collection add test

--- src/support/collection/str_collection.go
+++ src/support/collection/str_collection.go
@@ -7,26 +7,31 @@ import (
 	"sort"
 )
 
+// strCollection is a collection of strings.
 type strCollection struct {
 	items []string
 }
 
+// NewStrCollection creates a new string collection.
 func NewStrCollection(items []string) *strCollection {
 	return &strCollection{
 		items: items,
 	}
 }
 
+// Items returns the items in the collection.
 func (c *strCollection) Items() []string {
 	return c.items
 }
 
+// Add adds an item to the collection.
 func (c *strCollection) Add(item string) *strCollection {
 	c.items = append(c.items, item)
 
 	return c
 }
 
+// Remove removes the first occurrence of the given item from the collection.
 func (c *strCollection) Remove(item string) {
 	for i, v := range c.items {
 		if v == item {
@@ -36,6 +41,7 @@ func (c *strCollection) Remove(item string) {
 	}
 }
 
+// RemoveAll removes all specified items from the collection.
 func (c *strCollection) RemoveAll(item string) {
 	for i := 0; i < len(c.items); {
 		if c.items[i] == item {
@@ -46,6 +52,7 @@ func (c *strCollection) RemoveAll(item string) {
 	}
 }
 
+// Contains returns true if the collection contains the item.
 func (c *strCollection) Contains(item string) bool {
 	for _, v := range c.items {
 		if v == item {
@@ -56,115 +63,126 @@ func (c *strCollection) Contains(item string) bool {
 	return false
 }
 
+// Clear removes all items from the collection.
 func (c *strCollection) Clear() {
 	c.items = []string{}
 }
 
+// Size returns the length of the collection, alias Len().
 func (c *strCollection) Size() int {
 	return c.Len()
 }
 
+// Len returns the length of the collection.
 func (c *strCollection) Len() int {
 	return len(c.items)
 }
 
+// Count returns the length of the collection, alias Len().
 func (c *strCollection) Count() int {
 	return c.Len()
 }
 
+// IsEmpty returns true if the collection is empty.
 func (c *strCollection) IsEmpty() bool {
 	return len(c.items) == 0
 }
 
-func (c *strCollection) Each(fn func(string)) {
-	for _, v := range c.items {
-		fn(v)
+// Each iterates over the collection calling the callback for each item.
+func (c *strCollection) Each(fn func(int, string)) {
+	for i, v := range c.items {
+		fn(i, v)
 	}
 }
 
-func (c *strCollection) Map(fn func(string) string) []string {
-	var result []string
+// Map returns a new data with the results of calling the given function
+func (c *strCollection) Map(fn func(int, string) interface{}) []interface{} {
+	var result []interface{}
 
-	for _, v := range c.items {
-		result = append(result, fn(v))
+	for i, v := range c.items {
+		result = append(result, fn(i, v))
 	}
 
 	return result
 }
 
-func (c *strCollection) Filter(fn func(string) bool) []string {
+// Filter returns a new collection with items that return true for the given function.
+func (c *strCollection) Filter(fn func(int, string) bool) *strCollection {
 	var result []string
 
-	for _, v := range c.items {
-		if fn(v) {
+	for i, v := range c.items {
+		if fn(i, v) {
 			result = append(result, v)
 		}
 	}
 
-	return result
+	return NewStrCollection(result)
 }
 
-func (c *strCollection) Reject(fn func(string) bool) []string {
-	return c.Filter(func(v string) bool {
-		return !fn(v)
+// Reject returns a new collection with all items that do not match the given
+func (c *strCollection) Reject(fn func(int, string) bool) *strCollection {
+	return c.Filter(func(i int, v string) bool {
+		return !fn(i, v)
 	})
 }
 
-func (c *strCollection) Find(fn func(string) bool) string {
-	for _, v := range c.items {
-		if fn(v) {
-			return v
-		}
-	}
-	return ""
-}
-
-func (c *strCollection) FindIndex(fn func(string) bool) int {
+// Find returns the first item that satisfies the given predicate.
+func (c *strCollection) Find(fn func(int, string) bool) (int, string) {
 	for i, v := range c.items {
-		if fn(v) {
-			return i
+		if fn(i, v) {
+			return i, v
 		}
 	}
-	return -1
+
+	return -1, ""
 }
 
-func (c *strCollection) FindLast(fn func(string) bool) string {
+// FindIndex returns the index of the first item that satisfies the given predicate.
+func (c *strCollection) FindIndex(fn func(int, string) bool) int {
+	i, _ := c.Find(fn)
+
+	return i
+}
+
+// FindLast returns the last item that satisfies the given predicate.
+func (c *strCollection) FindLast(fn func(int, string) bool) (int, string) {
 	for i := len(c.items) - 1; i >= 0; i-- {
-		if fn(c.items[i]) {
-			return c.items[i]
+		if fn(i, c.items[i]) {
+			return i, c.items[i]
 		}
 	}
-	return ""
+
+	return -1, ""
 }
 
-func (c *strCollection) FindLastIndex(fn func(string) bool) int {
-	for i := len(c.items) - 1; i >= 0; i-- {
-		if fn(c.items[i]) {
-			return i
-		}
-	}
-	return -1
+// FindLastIndex returns the index of the last item that satisfies the given predicate.
+func (c *strCollection) FindLastIndex(fn func(int, string) bool) int {
+	i, _ := c.FindLast(fn)
+
+	return i
 }
 
-func (c *strCollection) Reduce(fn func(string, string, int) string, initial string) string {
-	var result string
+// Reduce applies a function against an accumulator and each item in the collection (from left-to-right) to reduce it to a single value.
+func (c *strCollection) Reduce(fn func(int, string, interface{}) interface{}, initial interface{}) interface{} {
+	var result interface{}
 
 	result = initial
 
 	for i, v := range c.items {
-		result = fn(result, v, i)
+		result = fn(i, v, result)
 	}
 
 	return result
 }
 
-func (c *strCollection) ReduceRight(fn func(string, string, int) string, initial string) string {
-	var result string
+// ReduceRight applies a function against an accumulator and each item in the collection (from right-to-left) to reduce it to a single value.
+func (c *strCollection) ReduceRight(fn func(int, string, interface{}) interface{}, initial interface{}) interface{} {
+	var result interface{}
 
 	result = initial
 
 	for i := len(c.items) - 1; i >= 0; i-- {
-		result = fn(result, c.items[i], i)
+		result = fn(i, c.items[i], result)
 	}
 
 	return result
--- src/support/collection/str_collection_test.go
+++ src/support/collection/str_collection_test.go
@@ -40,3 +40,164 @@ func TestStrCollection_Base(t *testing.T) {
 		"a", "c", "d",
 	}, c.Items())
 }
+
+func TestStrCollection_Contains(t *testing.T) {
+	c := NewStrCollection([]string{
+		"a", "b", "c",
+	})
+
+	// Contains
+	assert.True(t, c.Contains("a"))
+	assert.False(t, c.Contains("d"))
+}
+
+func TestStrCollection_ClearAndLenAndIsEmpty(t *testing.T) {
+	c := NewStrCollection([]string{
+		"a", "b", "c",
+	})
+
+	assert.False(t, c.IsEmpty())
+	assert.Equal(t, 3, c.Len())
+
+	// Clear
+	c.Clear()
+	assert.True(t, c.IsEmpty())
+	assert.Equal(t, 0, c.Len())
+}
+
+func TestStrCollection_Each(t *testing.T) {
+	c := NewStrCollection([]string{
+		"a", "b", "c",
+	})
+
+	// Each
+	c.Each(func(i int, v string) {
+		if i == 0 {
+			assert.Equal(t, "a", v)
+		} else if i == 1 {
+			assert.Equal(t, "b", v)
+		} else if i == 2 {
+			assert.Equal(t, "c", v)
+		}
+	})
+}
+
+func TestStrCollection_Map(t *testing.T) {
+	c := NewStrCollection([]string{
+		"a", "b", "c",
+	})
+
+	r := c.Map(func(i int, v string) interface{} {
+		return v + v
+	})
+
+	assert.Equal(t, []interface{}{
+		"aa", "bb", "cc",
+	}, r)
+}
+
+func TestStrCollection_Filter(t *testing.T) {
+	c := NewStrCollection([]string{
+		"a", "b", "c",
+	})
+
+	r1 := c.Filter(func(i int, v string) bool {
+		return v == "a"
+	})
+	r2 := c.Filter(func(i int, v string) bool {
+		return i == 1
+	})
+
+	assert.Equal(t, []string{
+		"a",
+	}, r1.Items())
+	assert.Equal(t, []string{
+		"b",
+	}, r2.Items())
+}
+
+func TestStrCollection_Reject(t *testing.T) {
+	c := NewStrCollection([]string{
+		"a", "b", "c",
+	})
+
+	r1 := c.Reject(func(i int, v string) bool {
+		return v == "a"
+	})
+	r2 := c.Reject(func(i int, v string) bool {
+		return i == 1
+	})
+
+	assert.Equal(t, []string{
+		"b", "c",
+	}, r1.Items())
+	assert.Equal(t, []string{
+		"a", "c",
+	}, r2.Items())
+}
+
+func TestStrCollection_FindAndFindIndex(t *testing.T) {
+	c := NewStrCollection([]string{
+		"a", "b", "c", "a",
+	})
+
+	// Find Use Value
+	index, value := c.Find(func(i int, v string) bool {
+		return v == "a"
+	})
+	assert.Equal(t, 0, index)
+	assert.Equal(t, "a", value)
+
+	// Find Use Index
+	index, value = c.Find(func(i int, v string) bool {
+		return i == 1
+	})
+
+	assert.Equal(t, 1, index)
+	assert.Equal(t, "b", value)
+
+	// FindIndex
+	index = c.FindIndex(func(i int, v string) bool {
+		return v == "c"
+	})
+	assert.Equal(t, 2, index)
+}
+
+func TestStrCollection_FindLastAndFindLastIndex(t *testing.T) {
+	c := NewStrCollection([]string{
+		"a", "b", "c", "a",
+	})
+
+	// FindLast Use Value
+	index, value := c.FindLast(func(i int, v string) bool {
+		return v == "a"
+	})
+	assert.Equal(t, 3, index)
+	assert.Equal(t, "a", value)
+
+	// FindLast Use Index
+	index, value = c.FindLast(func(i int, v string) bool {
+		return i == 1
+	})
+	assert.Equal(t, 1, index)
+	assert.Equal(t, "b", value)
+
+	// FindLastIndex
+	index = c.FindLastIndex(func(i int, v string) bool {
+		return v == "a"
+	})
+	assert.Equal(t, 3, index)
+}
+
+func TestStrCollection_Reduce(t *testing.T) {
+	c := NewStrCollection([]string{
+		"a", "b", "c",
+	})
+
+	// Reduce
+	r := c.Reduce(func(i int, v string, r interface{}) interface{} {
+		return r.(string) + v
+	}, "")
+	assert.Equal(t, "abc", r)
+	assert.Equal(t, "abc", r.(string))
+}
