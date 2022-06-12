56d4271cbe50065972a91c2ac18c284bd174fae3
Flc゛ <four_leaf_clover@foxmail.com>
2022-06-12 22:53:53 +0800

    optimize facades

--- support/facades/database.go
+++ support/facades/database.go
@@ -1,1 +1,28 @@
 package facades
+
+import (
+	"errors"
+	"github.com/go-packagist/framework/database"
+)
+
+// Database returns the database manager.
+func Database() (*database.Manager, error) {
+	db, err := App().Make("database")
+	if err != nil {
+		return nil, err
+	}
+
+	switch db.(type) {
+	case *database.Manager:
+		return db.(*database.Manager), nil
+	default:
+		return nil, errors.New("database is not a database manager")
+	}
+}
+
+// MustDatabase returns the database manager.
+func MustDatabase() *database.Manager {
+	db, _ := Database()
+
+	return db
+}
